b3f03bc7b65eec0cbc147d19a6bcd6207c0b5c14
MeelisP <meelis.perli@gmail.com>
2018-11-16 20:32:23 +0200

    Added a waypoint following npc

---
+++ Assets/Scripts/Patrol.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Patrol : MonoBehaviour {
+
+    public Transform[] points;
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+
+
+    void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        agent.autoBraking = false;
+        GotoNextPoint();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            GotoNextPoint();
+    }
+
+    void GotoNextPoint()
+    {
+        // Returns if no points have been set up
+        if (points.Length == 0)
+            return;
+
+        agent.destination = points[destPoint].position;
+
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+}
