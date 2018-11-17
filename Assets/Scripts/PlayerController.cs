1b94e409ed5d19333e408d0318cda8c3f9b8e878
Linde <kodaniklinde@gmail.com>
2018-11-18 01:39:27 +0200

    Sneak for player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -23,6 +23,11 @@ namespace Assets {
 
         // Update is called once per frame
         void Update() {
+            if(Input.GetKey(KeyCode.Escape)){
+                UnityEditor.EditorApplication.isPlaying = false; // For Linux can't quit game in editor
+                Application.Quit();
+            }
+            
             Vector3 movement = Vector3.zero;
             if (Input.GetKey(KeyCode.A)) {
                 movement = new Vector3(-1, movement.y, movement.z);
