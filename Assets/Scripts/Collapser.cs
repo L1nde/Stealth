a5ad64c2976a5404c330bae2ccc355b76fd7a547
L1nde <kodaniklinde@gmail.com>
2019-01-02 20:41:04 +0200

    Monitor is destroyable

---
+++ Assets/Scripts/Collapser.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collapser : MonoBehaviour {
+
+    public GameObject remains;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKey(KeyCode.F1)) {
+	        Instantiate(remains, transform.position, transform.rotation);
+	        Destroy(gameObject);
+	    }
+	}
+
+    private void OnCollisionEnter(Collision collision) {
+        Instantiate(remains, transform.position, transform.rotation);
+	    Destroy(gameObject);
+    }
+
+
+}
