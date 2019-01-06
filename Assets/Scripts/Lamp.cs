43eb30c0193f235e83442a5430bb35899aed4a0b
L1nde <kodaniklinde@gmail.com>
2019-01-06 15:08:32 +0200

    Lighting optimization. Now 60 Fps

--- Assets/Scripts/Lamp.cs
+++ Assets/Scripts/Lamp.cs
@@ -6,12 +6,20 @@ public class Lamp : MonoBehaviour {
 
     private GameObject darkness;
     private Light l;
-    
+    private Renderer renderer;
+
+    public Color emissiveColor;
+    public float intensity;
+
+    void Awake() {
+        renderer = GetComponent<Renderer>();
+    }
 
 	void Start () {
         darkness = transform.Find("Darkness").gameObject;
         l = GetComponentInChildren<Light>();
         darkness.SetActive(false);
+	    
 	}
 
     
@@ -22,12 +30,16 @@ public class Lamp : MonoBehaviour {
             if (darkness != null)
                 darkness.gameObject.SetActive(false);
         }
+        DynamicGI.SetEmissive(renderer, emissiveColor);
+        renderer.materials[1].SetColor("_EmissionColor", emissiveColor * intensity);
     }
 
     public void turnOff() {
         l.gameObject.SetActive(false);
         if (darkness != null)
             darkness.gameObject.SetActive(true);
+        DynamicGI.SetEmissive(renderer, Color.black);
+        renderer.materials[1].SetColor("_EmissionColor", Color.black);
     }
 
 }
