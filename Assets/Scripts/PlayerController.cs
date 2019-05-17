cd552f64fb49c36463121e280ab1b479c06f9e0e
L1nde <kodaniklinde@gmail.com>
2019-05-17 08:13:34 +0300

    Build order and exit fix

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -32,10 +32,6 @@ namespace Assets {
 
         // Update is called once per frame
         void Update() {
-            if(Input.GetKey(KeyCode.Escape)){
-
-                Application.Quit();
-            }
 
             if (Input.GetKey(KeyCode.BackQuote)) {
                 //UnityEditor.EditorApplication.isPlaying = false; // For Linux can't quit game in editor
